7660bdc186c447a8d89c6b11c5e94188b220f902
TrueXz <maskor.gpt@gmail.com>
2023-11-20 17:41:12 +0500

    Proverka Fork`a

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+// Proverka Forka
 public class SimpleCalculator {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
